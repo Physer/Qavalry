5536f4a4d8d5e086f6435f1b1e1fb956fe26a5f0
Alex Schouls <alex.schouls@valtech.com>
2019-12-06 15:46:08 +0100

    Made boilerplate runnable with Webdriver 5 (and such) - without HTML reporting

--- src/index.js
+++ src/index.js
@@ -8,49 +8,49 @@ import dateformat from 'dateformat';
 const launcher = require('@wdio/cli').default;
 const args = require('yargs').argv;
 const fsextra = require('fs-extra');
-var configFile = './wdio.defaults.conf.js';
+var configFile = './wdio.conf.js';
 var defaultOptionsFile = './configuration/options.js';
 var defaultReporterSettings = './configuration/reporterSettings.js';
 
 function prepareOutputFolders() {
     var outputFolder = path.join(process.cwd(), './_output');
-    console.log.info('Checking existence of output folder ' + outputFolder);
+    console.log('Checking existence of output folder ' + outputFolder);
     if (!fs.existsSync(outputFolder)) {
-        console.log.info('Creating output directory.');
+        console.log('Creating output directory.');
         fs.mkdirSync(outputFolder);
     }
 
     var logsFolder = path.join(process.cwd(), './_output/logs');
-    console.log.info('Checking existence of logs folder ' + logsFolder);
+    console.log('Checking existence of logs folder ' + logsFolder);
     if (!fs.existsSync(logsFolder)) {
-        console.log.info('Creating logs directory.');
+        console.log('Creating logs directory.');
         fs.mkdirSync(logsFolder);
     }
 
     var reportsFolder = path.join(process.cwd(), './_output/reports');
-    console.log.info('Checking existence of reports folder ' + reportsFolder);
+    console.log('Checking existence of reports folder ' + reportsFolder);
     if (!fs.existsSync(reportsFolder)) {
-        console.log.info('Creating reports directory.');
+        console.log('Creating reports directory.');
         fs.mkdirSync(reportsFolder);
     }
 
     var htmlReportsFolder = path.join(process.cwd(), './_output/reports/html');
-    console.log.info('Checking existence of html reports folder ' + htmlReportsFolder);
+    console.log('Checking existence of html reports folder ' + htmlReportsFolder);
     if (!fs.existsSync(htmlReportsFolder)) {
-        console.log.info('Creating html reports directory.');
+        console.log('Creating html reports directory.');
         fs.mkdirSync(htmlReportsFolder);
     }
 
     var jsonReport = path.join(process.cwd(), './_output/reports/_cucumber-report.json');
-    console.log.info('Checking existence of Cucumber report file ' + jsonReport);
+    console.log('Checking existence of Cucumber report file ' + jsonReport);
     if (fs.existsSync(jsonReport)) {
-        console.log.info('Cleaning Cucumber report files');
+        console.log('Cleaning Cucumber report files');
         fs.unlinkSync(jsonReport);
     }
 }
 
 function setup() {
-    console.log.info('Running setup!');
+    console.log('Running setup!');
     var destination = process.cwd();
     fsextra.copy(path.join(__dirname, '../boilerplate'), `${destination}`, err => {
         if (err) {
@@ -80,7 +80,7 @@ function createHtmlReport() {
             var customOptions = require(path.join(process.cwd(), args.reportdata)).config;
             options = Object.assign(options, customOptions);
         } else if(fs.existsSync(defaultReporterSettings)) {
-            console.log.info('Default reporter settings found at: ' + defaultReporterSettings);
+            console.log('Default reporter settings found at: ' + defaultReporterSettings);
             var defaultOptions = require(path.join(process.cwd(), defaultReporterSettings)).config;
             options = Object.assign(options, defaultOptions);
         }
@@ -99,7 +99,7 @@ if (process.argv[2] == 'setup') {
     if (args.options) {
         options = require(path.join(process.cwd(), args.options)).config;
     } else if (fs.existsSync(defaultOptionsFile)) {
-        console.log.info('Default options file found at: ' + defaultOptionsFile);
+        console.log('Default options file found at: ' + defaultOptionsFile);
         options = require(path.join(process.cwd(), defaultOptionsFile)).config;
     } else {
         console.log.error('No options file was specified and default options file not found at: ' + defaultOptionsFile);
@@ -119,20 +119,23 @@ if (process.argv[2] == 'setup') {
         options.cucumberOpts.tagExpression = args.tags;
     }
 
-    console.log.info('Registered tags: ' + options.cucumberOpts.tagExpression);
+    console.log('Registered tags: ' + options.cucumberOpts.tagExpression);
     // Run tests 
-    console.log.info('Running for site: ' + options.baseUrl);
+    console.log('Running for site: ' + options.baseUrl);
 
     // Start test run
     var wdio = new launcher(path.join(__dirname, configFile), options);
 
     // Start test
-    wdio.run().then((code) => {
-        log.info('Creating html report');
-        createHtmlReport();
+    wdio.run()
+    .then(
+      code => {
+        console.info('Launcher completed with code', code);
         process.exit(code);
-    }, function (error) {
-        console.log.error('Error while running the tests!');
-        console.log.error(error);
-    });
+      },
+      error => {
+        console.error('Launcher failed to start the test', error.stacktrace);
+        process.exit(1);
+      },
+    );
 }
