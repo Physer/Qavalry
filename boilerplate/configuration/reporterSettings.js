9a3c52a5dfe25a818a4c31070fbdc2cf9451d2b8
Alex Schouls <alex_schouls@live.com>
2019-12-20 13:52:27 +0100

    Dependency and file cleanup

--- boilerplate/configuration/reporterSettings.js
+++
@@ -1,7 +0,0 @@
-// For available options, please see
-// https://www.npmjs.com/package/cucumber-html-reporter
-module.exports.config = {
-    theme: 'bootstrap',
-    reportSuiteAsScenarios: false,
-    launchReport: false
-}
